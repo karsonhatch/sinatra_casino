c22eb98e25510501950cb280ee62d0d092a7ab95
Karson Hatch <karsonhatch@gmail.com>
2016-02-29 10:48:21 -0700

    Added a ton of stuff, too much to doctument, just download it.

--- home.rb
+++ home.rb
@@ -4,3 +4,16 @@ get '/' do
 	erb :casino_home
 end
 
+get '/numberguess' do 
+	erb :numberguess
+end
+
+get '/slots' do 
+	erb :slots 
+end
+
+# ========== POST ============
+
+post '/' do
+	params['name']
+end
